dbbc2bf39d5979593295e2852be0bd416798fb2b
Julie Barron <jbarron@wellesley.edu>
2013-10-18 00:51:32 -0400

    chapter 9 exercises

--- chap09/find_words.py
+++ chap09/find_words.py
@@ -1,11 +1,10 @@
-"""Test code for palindrome.py.
+"""Test code for find_words.py.
 
 Author: Allen B. Downey
 """
 
-def has_no_e(s):
-    return True
-
+def has_no_e(s): 
+    return not 'e' in s
 
 def main():
     for line in open('words.txt'):
@@ -14,9 +13,27 @@ def main():
         word = line.strip()
 
         # only print palindromes
-        if has_no_e(word):
-            print word
+        if has_no_e(word) and len(word) > 20: 
+            pass #so that it doesn't affect my other functions 
+            #print words
 
+def avoids(word, forbidden):
+    return not forbidden in word
+
+#print avoids('hello', 'ap')
+
+def uses_only(word, using):
+    for letter in word:
+        return not letter not in using
+
+print uses_only('face holey', 'acefhlo')
+
+def uses_all(word, required):
+    return uses_only(required, word)
+
+#print uses_all('haeli', 'aeiouy') something isn't right about this
 
 if __name__ == '__main__':
     main()
+
+
