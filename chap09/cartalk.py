282fb0e0f274706b470c3b1c2bd37effb1992e0e
Julie Barron <jbarron@wellesley.edu>
2013-12-05 23:19:43 -0500

    updates

--- chap09/cartalk.py
+++ chap09/cartalk.py
@@ -1,2 +1,13 @@
+def has_double_letter(word):
+    index = 0
+    while index < len(word)-1:
+        if word[index] == word[index+1]:
+            return True
+        index = index + 1
+    else:
+        return False
+def has_triple_double_letter(word):
+    count = 0
+    
 
-
+print has_double_letter('hejhjkhllo')
